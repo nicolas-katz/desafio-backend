3bd7847918d336fe5a031deee4273578e6dbc42c
nicolas-katz <nicokatz12@gmail.com>
2021-12-15 18:15:42 -0300

    arreglo del desafio

--- desafio4/index.js
+++ desafio4/index.js
@@ -1,85 +1,52 @@
 const express = require('express');
+const Products = require('./productsList');
 const { Router } = express;
-const Contenedor = require('./productsList.js');
-const user = new Contenedor('/productsList.js');
+const user = new Products("products.json");
 const app = express();
 const router = Router();
+require('dotenv').config();
 
 app.use(express.json())
-app.use(urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended : true }))
+app.use(express.static('./public'))
 app.use('/api/productos', router)
-app.use(express.static('public'))
 
 router.get('/', (req, res) =>{
-    try {
-        return res.json(user.getAll())
-    } catch(err) {
-        res.send({error: "Producto no encontrado"})
-    }
+    return res.json(user.list)
 })
 
-router.get('/:id', async (req, res) =>{
-    try {
-        let ID = Number(req.params.id)
-        const product = await user.getById(ID)
-        if (product === undefined) return res.send("ID incorrecto")
-        res.json(product)
-    } catch(err) {
-        res.send({error: "Producto no encontrado"})
-    }
+router.get('/:id', (req, res) =>{
+    let id = req.params.id
+    let product = user.find(id)
+    if (product === undefined) return res.send("ID incorrecto")
+    res.json(product)
 })
 
 router.post('/', async (req, res) =>{
-    try {
-        const product = req.body
-        const createProduct = {
-            title: product.title,
-            price: product.price,
-            thumbnail: product.thumbnail
-        }
-        await user.save(createProduct)
-        return res.redirect('/')
-    } catch(err){
-        res.send({error: "Producto no encontrado"})
+    let product = req.body
+    let createProduct = {
+        title: product.title,
+        price: product.price,
+        thumbnail: product.thumbnail
     }
+    return res.json(user.insert(createProduct))
 })
 
 router.put('/:id', (req, res) =>{
-    try {
-        let ID = Number(req.params.id)
-        const products = user.getAll()
-        if (ID < 1 || ID > products.length) return res.send('ID incorrecto')
-        const updateProduct = {
-            title: req.body.title,
-            price: req.body.price,
-            thumbnail: req.body.thumbnail,
-            id: ID
-        }
-        products[ID - 1] = updateProduct
-        res.json(products)
-    } catch(err) {
-        res.send({error: "Producto no encontrado"})
-    }
+    let product = req.body
+    let id = req.params.id
+    return res.json(user.update(id, product))
 })
 
-router.delete('/:id', async (req, res) =>{
-    try {
-        const ID = Number(req.params.id)
-        const delate = await user.deleteById(ID)
-        if (delate === undefined) return res.send("ID incorrecto")
-        res.json({delate})
-    } catch (err){
-        res.send({error: "Producto no encontrado"})
-    }
+router.delete('/:id', (req, res) =>{
+    let id = req.params.id
+    return res.json(user.delate(id))
+})
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 404).send({
+        error: "Ocurrio un error 4xx"
+    })  
 })
 
 app.listen(process.env.PORT || 8080)
-app.use(function(err, req, res, next) {
-    res.status(err.status || 404).send({
-        err: {
-        status: err.status || 404,
-        message: err.message || "Pagina no encontrada."
-        }
-    })  
-    next()
-})
